aba22b06671520dfab770080481bbb06caaa98c0
Mikolaj <m.lewandowski.0231@gmail.com>
2024-03-03 21:17:55 +0100

    feat: created about us section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import AboutUs from '@/components/organisms/home/about-us/about-us';
 import Hero from '@/components/organisms/home/hero/hero';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <AboutUs />
     </main>
   );
 }
