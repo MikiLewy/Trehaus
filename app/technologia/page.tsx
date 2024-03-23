114dc1fc792d03bb61468a80b253c44260e5a1e7
Mikolaj <m.lewandowski.0231@gmail.com>
2024-03-23 20:06:40 +0100

    feat: created content section

--- app/technologia/page.tsx
+++ app/technologia/page.tsx
@@ -1,9 +1,22 @@
 import PageHeader from '@/components/atoms/page-header/page-header';
+import ContentSection from '@/components/organisms/technology/content-section/content-section';
+import { technologySections } from '@/constants/technology-sections';
 
 const Technology = () => {
   return (
     <main>
       <PageHeader>Technologia</PageHeader>
+      <div className="flex flex-col my-4">
+        {technologySections.map((technologySection, index) => (
+          <ContentSection
+            key={technologySection.key}
+            title={technologySection.title}
+            description={technologySection.description}
+            img={technologySection.img}
+            direction={index % 2 === 0 ? 'right' : 'left'}
+          />
+        ))}
+      </div>
     </main>
   );
 };
