b753bedf323bccfb8f047bdcd0efa64f5b283f51
Mikolaj <m.lewandowski.0231@gmail.com>
2024-03-23 20:16:59 +0100

    feat: created offer page

---
+++ app/oferta/page.tsx
@@ -0,0 +1,11 @@
+import PageHeader from '@/components/atoms/page-header/page-header';
+
+const Offer = () => {
+  return (
+    <main>
+      <PageHeader>Oferta</PageHeader>
+    </main>
+  );
+};
+
+export default Offer;
