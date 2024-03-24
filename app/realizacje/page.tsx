0f5844fb8a9dd8c0253af1bb23212906c5c4cc38
Mikolaj <m.lewandowski.0231@gmail.com>
2024-03-24 12:12:36 +0100

    feat: created realizations page

---
+++ app/realizacje/page.tsx
@@ -0,0 +1,11 @@
+import PageHeader from '@/components/atoms/page-header/page-header';
+
+const Realizations = () => {
+  return (
+    <main>
+      <PageHeader>Realizacje</PageHeader>
+    </main>
+  );
+};
+
+export default Realizations;
