e184ce8cb2ee7c660134ca515b0df34908e21107
Mikolaj <m.lewandowski.0231@gmail.com>
2024-03-26 21:43:14 +0100

    feat: created contact page

---
+++ app/kontakt/page.tsx
@@ -0,0 +1,11 @@
+import PageHeader from '@/components/atoms/page-header/page-header';
+
+const Contact = () => {
+  return (
+    <main>
+      <PageHeader>Kontakt</PageHeader>
+    </main>
+  );
+};
+
+export default Contact;
