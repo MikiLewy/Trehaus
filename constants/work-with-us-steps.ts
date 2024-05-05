bd66703293957a4ca3fe83e06510a40804379263
MikiLewy <m.lewandowski.0231@gmail.com>
2024-05-05 16:20:27 +0200

    feat: changed styles of timeline card

--- constants/work-with-us-steps.ts
+++ constants/work-with-us-steps.ts
@@ -4,32 +4,47 @@ export const workWithUsSteps = [
     title: 'Wybór projektu',
     description:
       'Jeśli masz już wybrany projekt to świetnie, zbudujemy Twój dom według Twojej wizji. Jeśli nie masz jeszcze projektu, możesz wybrać jeden z naszych, dostępnych na tej stronie.',
-    img: '/work/project-selection.webp',
+    img: {
+      src: '/work/project-selection.webp',
+      alt: 'Wybór projektu domu',
+    },
   },
   {
     key: 'agreement',
     title: 'Formalności',
     description:
       'Po wyborze projektu oraz podpisaniu umowy, pomożemy w uzyskaniu warunków zabudowy oraz otrzymaniu pozwolenia na budowę.',
-    img: '/work/agreement.webp',
+    img: {
+      src: '/work/agreement.webp',
+      alt: 'Podpisanie umowy',
+    },
   },
   {
     key: 'preparation-of-the-construction',
     title: 'Przygotowanie terenu pod budowę',
     description: 'Pomożemy w uzbrojeniu działki w wodę oraz prąd budowlany.',
-    img: '/work/preparation-for-construction.webp',
+    img: {
+      src: '/work/preparation-for-construction.webp',
+      alt: 'Przygotowanie działki do budowy',
+    },
   },
   {
     key: 'construction',
     title: 'Budowa',
     description:
       'Wybudujemy dom zgodnie z projektem, terminem oraz zapisami ustalonymi w podpisanej umowie.',
-    img: '/work/construction.webp',
+    img: {
+      src: '/work/construction.webp',
+      alt: 'Budowa domu zgodnie z projektem',
+    },
   },
   {
     key: 'completion-of-construction',
     title: 'Zakończenie budowy',
     description: 'Przekazanie kluczy do domu nowym właścicielom.',
-    img: '/work/key-to-house.webp',
+    img: {
+      src: '/work/key-to-house.webp',
+      alt: 'Wręczenie kluczy do domu po zakończeniu budowy',
+    },
   },
 ];
