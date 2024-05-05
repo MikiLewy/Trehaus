0656dc9483d313295b9694ae692ef6b45e8461e5
Mikolaj <m.lewandowski.0231@gmail.com>
2024-05-05 15:43:40 +0200

    feat: changed images in work with us steps timeline

--- constants/work-with-us-steps.ts
+++ constants/work-with-us-steps.ts
@@ -1,35 +1,35 @@
 export const workWithUsSteps = [
   {
-    key: 'choose',
+    key: 'project-selection',
     title: 'Wybór projektu',
     description:
       'Jeśli masz już wybrany projekt to świetnie, zbudujemy Twój dom według Twojej wizji. Jeśli nie masz jeszcze projektu, możesz wybrać jeden z naszych, dostępnych na tej stronie.',
-    img: '/bilbo.webp',
+    img: '/work/project-selection.webp',
   },
   {
-    key: 'start2',
+    key: 'agreement',
     title: 'Formalności',
     description:
       'Po wyborze projektu oraz podpisaniu umowy, pomożemy w uzyskaniu warunków zabudowy oraz otrzymaniu pozwolenia na budowę.',
-    img: '/bilbo.webp',
+    img: '/work/agreement.webp',
   },
   {
-    key: 'start3',
+    key: 'preparation-of-the-construction',
     title: 'Przygotowanie terenu pod budowę',
     description: 'Pomożemy w uzbrojeniu działki w wodę oraz prąd budowlany.',
-    img: '/bilbo.webp',
+    img: '/work/preparation-for-construction.webp',
   },
   {
-    key: 'start4',
+    key: 'construction',
     title: 'Budowa',
     description:
       'Wybudujemy dom zgodnie z projektem, terminem oraz zapisami ustalonymi w podpisanej umowie.',
-    img: '/bilbo.webp',
+    img: '/work/construction.webp',
   },
   {
-    key: 'start5',
+    key: 'completion-of-construction',
     title: 'Zakończenie budowy',
     description: 'Przekazanie kluczy do domu nowym właścicielom.',
-    img: '/bilbo.webp',
+    img: '/work/key-to-house.webp',
   },
 ];
