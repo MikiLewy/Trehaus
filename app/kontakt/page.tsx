b2b81f8ae498a3dcdea08f0d6db2749fb0529a11
Mikolaj <m.lewandowski.0231@gmail.com>
2024-05-23 00:08:12 +0200

    feat: Added metadata tags to all pages, created manifest, robots, sitemap files, changed favicon and added opengraph image

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from 'next';
+
 import ContactSection from '@/features/contact/components/organisms/contact-section/contact-section';
+
+export const metadata: Metadata = {
+  title: 'Kontakt',
+  description:
+    'Skontaktuj się z nami, aby uzyskać więcej informacji o naszych projektach budowlanych. Jesteśmy do Twojej dyspozycji poprzez telefon, email lub formularz kontaktowy. Odpowiemy na wszystkie pytania i pomożemy w realizacji Twojego wymarzonego domu. Zapraszamy do kontaktu!',
+};
 
 const Contact = () => {
   return (
--- app/realizacje/page.tsx
+++ app/realizacje/page.tsx
@@ -3,10 +3,17 @@ import {
   HydrationBoundary,
   QueryClient,
 } from '@tanstack/react-query';
+import { Metadata } from 'next';
 
 import PageHeader from '@/components/atoms/page-header/page-header';
 import Realizations from '@/features/realizations/components/organisms/realizations';
 import { usePrefetchRealizations } from '@/features/realizations/hooks/api/realizations/use-prefetch-realizations';
+
+export const metadata: Metadata = {
+  title: 'Realizacje',
+  description:
+    'Zobacz nasze zrealizowane projekty budowlane, w tym domy energooszczędne i nowoczesne konstrukcje. Oferujemy wysoką jakość wykonania oraz innowacyjne technologie. Przekonaj się, jak nasze realizacje mogą spełnić Twoje marzenia o idealnym domu. Sprawdź naszą galerię projektów i zainspiruj się już dziś.',
+};
 
 const RealizationsPage = async () => {
   const queryClient = new QueryClient();
