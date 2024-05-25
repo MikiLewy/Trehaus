14183433e585eb9b0735e77450412ebd45fa83b9
Mikolaj <m.lewandowski.0231@gmail.com>
2024-05-25 17:53:32 +0200

    fix: added metadata to cookies page and updated sitemap file

--- app/info/cookies/page.tsx
+++ app/info/cookies/page.tsx
@@ -1,4 +1,15 @@
+import { Metadata } from 'next';
+
 import Typography from '@/components/atoms/typography/typography';
+
+export const metadata: Metadata = {
+  title: 'Polityka plików cookie',
+  description:
+    'Polityka plików cookie dla Trehaus. Dowiedz się, jakie pliki cookie używamy na naszej stronie internetowej i jakie dane zbieramy. Zapoznaj się z naszą polityką prywatności i dowiedz się, jakie prawa przysługują Ci jako użytkownikowi.',
+  robots: {
+    index: false,
+  },
+};
 
 const Page = () => {
   return (
