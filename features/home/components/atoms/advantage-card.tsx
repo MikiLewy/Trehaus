e757de4d93ac9dd742fdb0968bd0cdb160be9503
Mikolaj <m.lewandowski.0231@gmail.com>
2024-06-23 17:44:47 +0200

    fix: ui and ux fixes and adjustments

--- features/home/components/atoms/advantage-card.tsx
+++ features/home/components/atoms/advantage-card.tsx
@@ -10,9 +10,9 @@ interface Props {
 
 const AdvantageCard = ({ title, description, Icon }: Props) => {
   return (
-    <div className="flex flex-row gap-2 items-start justify-center  lg:py-8 text-black lg:gap-4">
+    <div className="flex flex-row flex-wrap gap-2 items-start lg:py-8 text-black lg:gap-4">
       <Icon className="h-8 w-8 xl:h-9 xl:w-9 min-w-max" />
-      <div className="flex flex-col">
+      <div className="flex flex-col flex-1">
         <Typography variant="h5">{title}</Typography>
         <Typography variant="p1" className="text-secondary mt-2">
           {description}
